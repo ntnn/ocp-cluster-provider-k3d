2aadee7613d06dd78fea0a228819e724bc9dfbb3
Nelo-T. Wallus <n.wallus@sap.com>
2026-09-03 13:14:39 +0200

    Add v1alpha1.ClusterStatus
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

---
+++ api/v1alpha1/cluster_types.go
@@ -0,0 +1,27 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// ClusterStatus is the provider-specific status for a k3d cluster.
+type ClusterStatus struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// K3dClusterName is the name of the underlying k3d cluster.
+	K3dClusterName string `json:"k3dClusterName"`
+}
