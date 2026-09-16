75181846be4dd116d540d8038f3dad8efb3d41f7
Nelo-T. Wallus <n.wallus@sap.com>
2026-09-16 17:06:02 +0200

    Honour kind annotation used by ocpctl
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- internal/controller/cluster/name.go
+++ internal/controller/cluster/name.go
@@ -11,10 +11,18 @@ import (
 // AnnotationName overrides the name of the backing k3d cluster.
 var AnnotationName = v1alpha1.GroupVersion.Group + "/name"
 
+// ocpctl hardcodes the kind annotation, so for cluster-provider-k3d to
+// work with ocpctl's local env it needs to honour the kind annotation
+// as well.
+const AnnotationNameKind = "kind.clusters.openmcp.cloud/name"
+
 // K3dName returns the name of the k3d cluster backing the given Cluster.
-// The AnnotationName annotation overrides the derived name.
+// AnnotationName, then AnnotationNameKind, override the derived name.
 func K3dName(cluster *clustersv1alpha1.Cluster) string {
 	if name, ok := cluster.Annotations[AnnotationName]; ok {
+		return name
+	}
+	if name, ok := cluster.Annotations[AnnotationNameKind]; ok {
 		return name
 	}
 	return fmt.Sprintf("%s-%s", cluster.Name, string(cluster.UID)[:8])
